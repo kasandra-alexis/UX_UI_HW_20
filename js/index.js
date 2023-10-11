c342736cdc5809f97515eebe34290ec1e5936e48
kasandra-alexis <132672839+kasandra-alexis@users.noreply.github.com>
2023-10-11 18:37:02 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(".progress-bar").animate({
+    width: "70%",
+}, 2500);
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
